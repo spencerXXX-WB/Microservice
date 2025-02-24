6ff41bfd9f16d8b9198f8aebf9c0176d94d7a52b
Spencer John <activegreatmind@gmail.com>
2025-02-24 10:46:32 +0000

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -103,9 +103,9 @@ resource "aws_eks_node_group" "devopsshack" {
   subnet_ids      = aws_subnet.devopsshack_subnet[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   instance_types = ["t2.small"]
